537e06d84a8ad5eeb163de86579ab663e09b3563
lyf44 <luyunfan44@gmail.com>
2021-08-27 21:26:39 +0800

    Extract state-dependent local connectable bottlenecks

--- bottleneck_node/extract_bottleneck.py
+++ bottleneck_node/extract_bottleneck.py
@@ -14,46 +14,9 @@ import os
 import copy
 import math
 
+import lego.utils as utils
+
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
-
-def is_edge_free(maze, node1_state, node2_state, EDGE_DISCRETIZATION = 20, inc = 0.035):
-    node1_pos = helper.state_to_numpy(node1_state)
-    node2_pos = helper.state_to_numpy(node2_state)
-    diff = node2_pos - node1_pos
-    edge_discretization = int(np.max(np.abs(diff)) / 0.1) + 1
-    step = diff / edge_discretization
-    assert(np.max(step) < 0.1 and np.min(step) > -0.1)
-
-    for i in range(edge_discretization):
-        nodepos = node1_pos + step * i
-        if(not maze.pb_ompl_interface.is_state_valid(nodepos.tolist())):
-            return 0
-    return 1
-
-def visualize_nodes(occ_g, curr_node_posns, start_pos, goal_pos):
-    fig1 = plt.figure(figsize=(10,6), dpi=80)
-    ax1 = fig1.add_subplot(111, aspect='equal')
-
-    for i in range(10):
-        for j in range(10):
-            if occ_g[i,j] == 1:
-                # ax1.add_patch(patches.Rectangle(
-                #     (i/10.0 - 2.5, j/10.0 - 2.5),   # (x,y)
-                #     0.1,          # width
-                #     0.1,          # height
-                #     alpha=0.6
-                #     ))
-                plt.scatter(j/2.0 - 2.25, 2.25 - i/2.0, color="black", marker='s', s=1000, alpha=1) # init
-    plt.scatter(start_pos[0], start_pos[1], color="red", s=100, edgecolors='black', alpha=1, zorder=10) # init
-    plt.scatter(goal_pos[0], goal_pos[1], color="blue", s=100, edgecolors='black', alpha=1, zorder=10) # goal
-
-    curr_node_posns = np.array(curr_node_posns)
-    if len(curr_node_posns)>0:
-        plt.scatter(curr_node_posns[:,0], curr_node_posns[:,1], s = 50, color = 'green')
-    plt.title("Visualization")
-    plt.xlim(-2.5, 2.5)
-    plt.ylim(-2.5, 2.5)
-    plt.show()
 
 def get_bottleneck_nodes(maze, dense_G, sparse_G, path_nodes, occ_grid, dis, row, col, inc):
     assert len(path_nodes)>0
@@ -82,7 +45,7 @@ def get_bottleneck_nodes(maze, dense_G, sparse_G, path_nodes, occ_grid, dis, row
                 continue
 
             # if helper.calc_weight_states(s1, s2) < THRESH:
-            if is_edge_free(maze, s1, s2):
+            if utils.is_edge_free(maze, s1, s2):
                 # n1 = node
                 # n2 = 'p'+str(count)
                 sparse_G.add_edge(node, path_node)
@@ -163,37 +126,72 @@ def test2(maze, occ_grid, dense_G, sparse_G, start_n):
 
     bottleneck_nodes, curr_path_nodes = get_bottleneck_nodes(maze, dense_G, sparse_G, path_nodes, occ_grid, dis, row, col, inc = INC)
     print("bottleneck_nodes = ", bottleneck_nodes)
-    points_x = []
-    points_y = []
+    states = []
     print("path_nodes = ", path_nodes, curr_path_nodes)
-    for node in path_nodes:
-        s = helper.state_to_numpy(dense_G.nodes[node]['coords'])
-        points_x.append(s[0])
-        points_y.append(s[1])
+    for node in curr_path_nodes:
+        s = helper.state_to_numpy(sparse_G.nodes[node]['coords'])
+        states.append(s)
 
-    # visualize_nodes(occ_grid, np.array(list(zip(points_x,points_y))), start_pos, goal_pos)
+    # utils.visualize_nodes(occ_grid, states, start_pos, goal_pos)
 
     points_x = []
     points_y = []
     ss = []
-    # bottleneck_nodes = bottleneck_nodes[1:-1] # remove start and goal
-    # if len(bottleneck_nodes) == 0:
-    #     bottleneck_nodes.append(curr_path_nodes[1]) # append second node in curr_path
 
-    # handle the corner case where the start node is in the sparse graph and hence the first two nodes in path are the same
+    # remove start and goal
     s1 = sparse_G.nodes[curr_path_nodes[0]]['coords']
     s2 = sparse_G.nodes[curr_path_nodes[1]]['coords']
     if helper.calc_weight_states(s1, s2) < 0.1:
-        bottleneck_nodes  = [curr_path_nodes[2]]
+        curr_path_nodes = curr_path_nodes[2:]
     else:
-        bottleneck_nodes  = [curr_path_nodes[1]]
+        curr_path_nodes = curr_path_nodes[1:]
+    if len(curr_path_nodes) > 1:
+        s1 = sparse_G.nodes[curr_path_nodes[-1]]['coords']
+        s2 = sparse_G.nodes[curr_path_nodes[-2]]['coords']
+        if helper.calc_weight_states(s1, s2) < 0.1:
+            curr_path_nodes = curr_path_nodes[:-2]
+        else:
+            curr_path_nodes = curr_path_nodes[:-1]
+    else:
+        curr_path_nodes = curr_path_nodes[:-1]
+
+    if len(curr_path_nodes) == 0:
+        # bottleneck_nodes.append(curr_path_nodes[1]) # append second node in curr_path
+        return goal_pos, ss
+
+    # # handle the corner case where the start node is in the sparse graph and hence the first two nodes in path are the same
+    # s1 = sparse_G.nodes[curr_path_nodes[0]]['coords']
+    # s2 = sparse_G.nodes[curr_path_nodes[1]]['coords']
+    # if helper.calc_weight_states(s1, s2) < 0.1:
+    #     bottleneck_nodes  = [curr_path_nodes[2]]
+
+    #     # sanity check
+    #     # if len(curr_path_nodes) > 3:
+    #     #     assert not sparse_G.has_edge(curr_path_nodes[1], curr_path_nodes[3])
+    #     #     s1 = sparse_G.nodes[curr_path_nodes[1]]['coords']
+    #     #     s2 = sparse_G.nodes[curr_path_nodes[3]]['coords']
+    #     #     assert not utils.is_edge_free(maze, s1, s2)
+    # else:
+    #     bottleneck_nodes  = [curr_path_nodes[1]]
+
+    #     # sanity check
+    #     # if len(curr_path_nodes) > 2:
+    #     #     assert not sparse_G.has_edge(curr_path_nodes[0], curr_path_nodes[2])
+    #     #     s1 = sparse_G.nodes[curr_path_nodes[0]]['coords']
+    #     #     s2 = sparse_G.nodes[curr_path_nodes[2]]['coords']
+    #     #     assert not utils.is_edge_free(maze, s1, s2)
+
+    # get bottleneck nodes
+    bottleneck_nodes = [curr_path_nodes[0]]
     print("path_nodes = ", path_nodes, bottleneck_nodes)
     for node in bottleneck_nodes:
-        s = helper.state_to_numpy(sparse_G.nodes[node]['coords'])
-        points_x.append(s[0])
-        points_y.append(s[1])
-        ss.append(s)
+        if 'p' in node:
+            s = helper.state_to_numpy(sparse_G.nodes[node]['coords'])
+            points_x.append(s[0])
+            points_y.append(s[1])
+            ss.append(s)
 
+    print("final bottleneck nodes: {}".format(ss))
     # visualize_nodes(occ_grid, np.array(list(zip(points_x,points_y))), start_pos, goal_pos)
     return goal_pos, ss
 
@@ -219,7 +217,7 @@ def main():
         points_x.append(s[0])
         points_y.append(s[1])
 
-    visualize_nodes(occ_grid,np.array(list(zip(points_x,points_y))), start_pos, goal_pos)
+    # visualize_nodes(occ_grid,np.array(list(zip(points_x,points_y))), start_pos, goal_pos)
 
     points_x = []
     points_y = []
@@ -229,7 +227,7 @@ def main():
         points_x.append(s[0])
         points_y.append(s[1])
 
-    visualize_nodes(occ_grid,np.array(list(zip(points_x,points_y))), start_pos, goal_pos)
+    # visualize_nodes(occ_grid,np.array(list(zip(points_x,points_y))), start_pos, goal_pos)
 
 if __name__ == '__main__':
     main()
